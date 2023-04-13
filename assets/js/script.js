150ce11ccc54645cac847fc7ab052d5fc4329f06
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-13 17:14:41 -0300

    consertando o bug da seção valores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 window.addEventListener('scroll', () => {
     let header = document.querySelector('header');
-        if (window.innerWidth >= 1000) {
             header.classList.toggle('sticky', window.scrollY > 0)
-        } 
 })
