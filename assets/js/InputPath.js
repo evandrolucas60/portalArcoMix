f9a46b2a5a90a3d2244204ab064e1987f12e8048
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-04-19 21:41:24 -0300

    incluindo o cógigo js do inputPath

--- assets/js/InputPath.js
+++ assets/js/InputPath.js
@@ -1,33 +1,52 @@
+/* Lê o arquivo .xlsx */
+const filePath = './assets/archives/Rota.xlsx';
+const req = new XMLHttpRequest();
+req.open('GET', filePath, true);
+req.responseType = 'arraybuffer';
+req.onload = function(e) {
+  const data = new Uint8Array(req.response);
 
-    const url = "./assets/archives/Rota.xlsx";
-    const inputFile = document.getElementById("inputFile");
-    
-    
-    // Define o caminho padrão como o valor do campo de entrada de arquivo
-    inputFile.nodeValue = caminhoPadrao
-    
-    // Simula um clique no botão "escolher arquivo"
-    inputFile.click();
+  /* Lê a planilha */
+  const workbook = XLSX.read(data, { type: 'array' });
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-    function loadURLToInputFiled(url){
-  getImgURL(url, (imgBlob)=>{
-    // Load img blob to input
-    // WIP: UTF8 character error
-    let fileName = 'hasFilename.jpg'
-    let file = new File([imgBlob], fileName,{type:"file/xlsx", lastModified:new Date().getTime()}, 'utf-8');
-    let container = new DataTransfer(); 
-    container.items.add(file);
-    document.querySelector('#inputFile').files = container.files;
-    
-  })
-}
-// xmlHTTP return blob respond
-function getImgURL(url, callback){
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-      callback(xhr.response);
-  };
-  xhr.open('GET', url);
-  xhr.responseType = 'blob';
-  xhr.send();
-}
+  /* Salva os dados no localStorage */
+  const dataKey = 'meus_dados';
+  const jsonDataString = JSON.stringify(jsonData);
+  localStorage.setItem(dataKey, jsonDataString);
+
+  /* Cria a tabela HTML */
+  const table = document.createElement('table');
+  table.border = '1';
+
+  /* Cria o cabeçalho da tabela */
+  const thead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+  for (let i = 0; i < jsonData[0].length; i++) {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = jsonData[0][i];
+    headerRow.appendChild(headerCell);
+  }
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  /* Cria o corpo da tabela */
+  const tbody = document.createElement('tbody');
+  for (let i = 1; i < jsonData.length; i++) {
+    const dataRow = document.createElement('tr');
+    for (let j = 0; j < jsonData[i].length; j++) {
+      const dataCell = document.createElement('td');
+      dataCell.textContent = jsonData[i][j];
+      dataRow.appendChild(dataCell);
+    }
+    tbody.appendChild(dataRow);
+  }
+  table.appendChild(tbody);
+
+  /* Insere a tabela na página */
+  const tabela = document.querySelector('#tabela');
+  tabela.appendChild(table);
+};
+req.send();
